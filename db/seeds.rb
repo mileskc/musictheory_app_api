e918e8c969afc13de2e0256e6428ce33a218c50d
Katelyn <katelyncmiles@gmail.com>
2019-09-09 16:21:20 -0400

    seeded answers for first question for first quiz and successfully linked to the question

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 
 # Quiz.create([{title: "basic intervals", description: "Identify basic intervals (m2 - M7) based on treble clef staff notation", img_url: "https://2.bp.blogspot.com/-8puHOjL95po/WOTCW2K1keI/AAAAAAAAAj8/tyNZP4_gT8oochfnOHpEtHmJGoANae6KwCLcB/s1600/harmonic%2Bintervals.png"}])
 
-Question.create([{img_url: "https://i.imgur.com/ecdRsyj.png", instructions: "Which harmonic interval is displayed in the image? Select the correct option below and submit your response.", quiz_id: 1}])
+# Question.create([{img_url: "https://i.imgur.com/ecdRsyj.png", instructions: "Which harmonic interval is displayed in the image? Select the correct option below and submit your response.", quiz_id: 1}])
+
+Answer.create([{content: "m6", is_correct: false, question_id: 1}, {content: "P5", is_correct: false, question_id: 1}, {content: "M6", is_correct: true, question_id: 1}, {content: "m7", is_correct: false, question_id: 1}])
